b96be8c632f612c34904ec6ba9b154390a8a1b69
Moniruzzaman Saikat <moniruzzamansaikat@yahoo.com>
2022-11-24 00:24:43 +0600

    last-negative-one

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# photography-lp
+# Brazzy Photo Graphy
+
+This one is for a photography website.
+`live-sass-compiler` for compiling sass.
+Live url: https://monirsaikat.github.io/netflix-landing-page/
+![Demo](./demo.png)
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,16 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'heading': ['Righteous', 'cursive']
+      },
       backgroundImage: {
-        'pricing-bg': './'
+        'pricing-bg': `url('./img/pricing.jpg')`
       },
       colors: {
-        primary: '#cc8e75'
+        primary: '#cc8e75',
+        overlay: `rgba(0,0,0,0.7)`
       }
     },
   },
